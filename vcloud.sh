77b45480a89b762659d4006396fb618d955239df
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-06-24 06:17:11 +0330

    Update vcloud.sh

--- vcloud.sh
+++ vcloud.sh
@@ -15,7 +15,7 @@ setup_vpncloud() {
     sudo sed -i "/^peers:/a\  - \"$remote_ip\"" "$CONFIG_FILE" || { echo "Failed to add peer"; exit 1; }
     restart_service
   else
-    read -p "Private IP e.g 10.0.50.1: " private_ip
+    read -p "Private IP e.g 10.0.50.1 : " private_ip
     sudo tee "$CONFIG_FILE" > /dev/null << EOF
 ---
 device:
