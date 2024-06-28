8dbc5e7a2adfef78aa489222f89516f250a18d89
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-06-28 14:38:00 +0330

    Update vcloud.sh

--- vcloud.sh
+++ vcloud.sh
@@ -3,7 +3,6 @@
 CONFIG_FILE="/etc/vpncloud/expert.net"
 
 Install_vpncloud() {
-    echo "deb https://repo.ddswd.de/deb stable main" | sudo tee /etc/apt/sources.list.d/vpncloud.list
     wget https://repo.ddswd.de/deb/public.key -qO - | sudo apt-key add
     sudo apt update
     sudo apt install vpncloud
